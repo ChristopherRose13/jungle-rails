561bca335b3577fcdf527bceb88da0e95784cb68
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-03-10 16:00:38 -0800

    add admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
+  get '/about', to: 'about#index'
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
